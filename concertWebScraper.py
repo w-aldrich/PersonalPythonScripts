5e0020cbdcf5dfb3f5b256c9d28b7851046a2a07
w-aldrich <william.aldrich.63@gmail.com>
2018-05-17 08:06:39 -0600

    update with comments

--- concertWebScraper.py
+++ concertWebScraper.py
@@ -1,5 +1,5 @@
 #Use beautiful soup and mechanize if certain website has javascript use selenium for that website
-import mechanize, subprocess
+import subprocess
 from bs4 import BeautifulSoup
 
 smithstix = subprocess.check_output("curl -s \"https://www.smithstix.com/music\"", shell=True)
@@ -8,18 +8,27 @@ events = soup.find_all("div", {"class": "event-row"})
 
 
 for allEvents in events:
+    # Grab the event title
     title = allEvents.find("div", {"class": "event-title"}).get_text()
+    # Get the month day and year of the concert
     day = allEvents.find("div", {"class": "day-week-container"}).get_text().replace("\n", "").replace(" ", "")
     day += ' ' + allEvents.find("div", {"class": "month-container"}).get_text().replace("\n", "").replace(" ", "")
     day += ' ' + allEvents.find("div", {"class": "day-container"}).get_text().replace("\n", "").replace(" ", "")
     day += ' ' + allEvents.find("div", {"class": "year-container"}).get_text().replace("\n", "").replace(" ", "")
+    # Get what venue it is at
     venue = ' ' + allEvents.find("div", {"class": "event-venue"}).get_text()
+    # Get the price of the concert
     getPrice = allEvents.find("div", {"class": "price"})
     price = ''
+
+    # ignore the ascii errors this allows to grab the text
+    # if the ascii encoding doesnt work skip it
     try:
         getPrice = getPrice.encode('ascii',errors='ignore')
     except AttributeError:
         continue
+
+    # Print the text needed 
     beginFlag = False
     for char in getPrice:
         if char == '>':
--- headlinesWebCrawler.py
+++ headlinesWebCrawler.py
@@ -1,6 +1,14 @@
 import subprocess
 from bs4 import BeautifulSoup
 
+'''
+This program will scrape nytimes.com and aljazeera.com
+for the headlines I care about looking at
+'''
+
+# Using curl and beautiful soup for both of these sites
+
+# NYTIMES
 htmlNytimes = subprocess.check_output("curl -s \"https://www.nytimes.com\"", shell=True)
 soup = BeautifulSoup(htmlNytimes, "html5lib")
 nytimesHeadlines = soup.find_all('a')
@@ -8,6 +16,7 @@ nytimesHeadlines = soup.find_all('a')
 nytimesText = []
 headlineCount = 0
 startHeadlines = False
+# loop through all 'a' tags remove unneeded white space and grab the correct headlines
 for headline in nytimesHeadlines:
     headline = headline.get_text().replace("\n", "").replace("\r", "").replace("\t", "")
     headline = headline.encode('ascii',errors='ignore')
@@ -21,20 +30,28 @@ for headline in nytimesHeadlines:
             break
         nytimesText.append(headline)
         headlineCount += 1
+
 print "\nNEW YOURK TIMES TOP 10 HEADLINES"
-headlineCount = 1
+
 for blah in nytimesText:
     print str(headlineCount) + ": " + blah
-    headlineCount += 1
 
+
+
+# ALJAZEERA
 htmlAljazeera = subprocess.check_output("curl -s \"https://www.aljazeera.com/news/\"", shell=True)
 soup = BeautifulSoup(htmlAljazeera, "html5lib")
+
+# Use soup to find all of the 'a' tags, these contain where the stories are from
 topic = soup.find_all('a', {"class": "topics-sec-item-label"})
+# Use soup to find all the 'h2' tags, these contain the actual headline
 alHeadlines = soup.find_all('h2', {"class": "topics-sec-item-head"})
 
 topicHeadlines = []
 for x in topic:
     topicHeadlines.append(x.get_text())
+
 print "\nALJAZERA OVERVIEW OF WORLD NEWS"
+
 for place, headline in zip(topicHeadlines, alHeadlines):
     print place + ": ", headline.get_text()
